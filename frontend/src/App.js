011aa7c2ca49c34f1d482adc63b9f197fdc50ead
Daniel Elliot <daniel@goodinc.co>
2022-12-01 17:16:19 -0800

    Added bootstrap and a navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import './App.css';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Hey fuuckers</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
